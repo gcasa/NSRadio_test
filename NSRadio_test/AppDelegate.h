c214e55d496bf199bc54bdc1f63ba6176dfe071c
Gregory John Casamento <greg.casamento@gmail.com>
2022-08-25 13:54:53 -0400

    Initial Commit

---
+++ NSRadio_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSRadio_test
+//
+//  Created by Gregory Casamento on 8/25/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
